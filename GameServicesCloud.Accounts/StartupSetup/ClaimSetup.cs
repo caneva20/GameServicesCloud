3b48a849972e12987dc00e973914924654ee3dca
Fabricio Caneva <hardlymersk@gmail.com>
2023-10-13 16:29:49 -0300

    Refactor Startup Setup for Claims
    Refactored the startup setup process for account claims to improve dependency injection. Moved the methods into StartupSetupExtensions and ClaimSetup classes for better separation of concern. Instead of retrieving dependencies such as ILogger and IRepository in methods, now they are being injected via the constructor in ClaimSetup class, which results in cleaner, more testable code. Changes aim to increase maintainability and testability.

--- GameServicesCloud.Accounts/StartupSetup/ClaimSetup.cs
+++ GameServicesCloud.Accounts/StartupSetup/ClaimSetup.cs
@@ -2,28 +2,31 @@
 
 namespace GameServicesCloud.Accounts.StartupSetup;
 
-public static class ClaimSetup {
-    public static async Task SetupClaims(this IApplicationBuilder app) {
-        using var scope = app.ApplicationServices.CreateScope();
+public class ClaimSetup {
+    private readonly ILogger<ClaimSetup> _logger;
+    private readonly IRepository<AccountClaim> _repository;
 
-        var logger = scope.ServiceProvider.GetRequiredService<ILogger<object>>();
-        var repository = scope.ServiceProvider.GetRequiredService<IRepository<AccountClaim>>();
+    public ClaimSetup(ILogger<ClaimSetup> logger, IRepository<AccountClaim> repository) {
+        _logger = logger;
+        _repository = repository;
+    }
 
-        logger.LogInformation("Updating claim register");
+    public async Task Run() {
+        _logger.LogInformation("Updating claim register");
 
-        var registeredClaims = await repository.FindAll();
+        var registeredClaims = await _repository.FindAll();
 
         var newClaims = Claims.AllClaims.Except(registeredClaims.Select(x => x.Name)).ToList();
         var oldClaims = registeredClaims.Where(x => !Claims.AllClaims.Contains(x.Name)).ToList();
 
-        logger.LogInformation("Creating {Quantity} new claims: {@Claims}", newClaims.Count, newClaims);
-        logger.LogInformation("Removing {Quantity} old claims: {@Claims}", oldClaims.Count, oldClaims.Select(x => x.Name));
+        _logger.LogInformation("Creating {Quantity} new claims: {@Claims}", newClaims.Count, newClaims);
+        _logger.LogInformation("Removing {Quantity} old claims: {@Claims}", oldClaims.Count, oldClaims.Select(x => x.Name));
 
         try {
-            await repository.SaveAll(newClaims.Select(x => new AccountClaim { Name = x }));
-            await repository.RemoveAll(oldClaims);
+            await _repository.SaveAll(newClaims.Select(x => new AccountClaim { Name = x }));
+            await _repository.RemoveAll(oldClaims);
         } catch (Exception e) {
-            logger.LogCritical(e, "Failed to update claim register!!");
+            _logger.LogCritical(e, "Failed to update claim register!!");
         }
     }
 }
---
+++ GameServicesCloud.Accounts/StartupSetup/StartupSetupExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace GameServicesCloud.Accounts.StartupSetup;
+
+public static class StartupSetupExtensions {
+    public static async Task SetupClaims(this IApplicationBuilder app) {
+        using var scope = app.ApplicationServices.CreateScope();
+
+        await ActivatorUtilities.CreateInstance<ClaimSetup>(scope.ServiceProvider).Run();
+    }
+}
