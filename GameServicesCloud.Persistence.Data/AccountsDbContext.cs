33015ec84c0838363916154a4583421d1bd74e7a
Fabricio Caneva <hardlymersk@gmail.com>
2023-10-15 17:50:26 -0300

    Add Persistence module
    Introduced a new Persistence project to the solution which contains persistence logic and configuration. This project also has its corresponding Dockerfile, enabling Docker support for containerization.

---
+++ GameServicesCloud.Persistence.Data/AccountsDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GameServicesCloud.Persistence;
+
+public class PersistenceDbContext : DbContext {
+    public PersistenceDbContext(DbContextOptions<PersistenceDbContext> options) : base(options) {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+    }
+}
---
+++ GameServicesCloud.Persistence.Data/PersistenceDataExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GameServicesCloud.Persistence;
+
+public static class PersistenceDataExtensions {
+    public static void AddData(this IServiceCollection services, string connectionString) {
+        services.AddDbContext<DbContext, PersistenceDbContext>(options => {
+            options.UseSqlServer(connectionString, x => x.MigrationsAssembly(typeof(PersistenceDataExtensions).Assembly.FullName));
+        });
+    }
+}
