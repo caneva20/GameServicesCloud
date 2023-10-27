91e3ca9b4bffaf29fbfc1bba9069e29039e19634
Fabricio Caneva <hardlymersk@gmail.com>
2023-10-27 16:23:50 -0300

    Add DataUnitConverter utility and update project settings
    This commit includes a new DataUnitConverter utility that converts byte data into human-readable format. We often need to display data sizes in the UI and the conversions were done inconsistently throughout the codebase, this utility standardizes that process. Additionally, the project settings were updated to ignore the 'utils' namespace during code inspection, following the already established pattern for 'models'.

---
+++ GameServicesCloud.UI/Utils/DataUnitConverter.cs
@@ -0,0 +1,15 @@
+﻿namespace GameServicesCloud.UI;
+
+public static class DataUnitConverter {
+    private static readonly string[] SizeUnits = { "Bytes", "KiB", "MiB", "GiB", "TiB" };
+
+    public static string Convert(double bytes) {
+        if (bytes <= 0) {
+            return "0 " + SizeUnits[0];
+        }
+
+        var digitGroups = (int)(Math.Log10(bytes) / Math.Log10(1024));
+
+        return $"{bytes / Math.Pow(1024, digitGroups):F1} {SizeUnits[digitGroups]}";
+    }
+}
